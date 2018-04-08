c69df7788178d630200cfd40b1e3cffd46bf1e2e
Evan Dean <evan.dean@intersection.com>
2018-04-08 18:06:48 -0400

    Start body layout

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 
 const style = {
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr 1fr',
+  gridTemplateRows: '404px 1fr',
+  gridTemplateAreas: `
+    'graph graph graph'
+    'list list list'
+  `,
+  gridGap: 30,
   backgroundColor: '#f0f3f4',
+  color: '#88909a',
+  padding: 30,
 };
+
+const listLayoutStyle = {
+  gridColumnStart: 1,
+  gridColumnEnd: 4,
+};
+
+const Graph = () => (
+  <div style={{ backgroundColor: 'white' }}>
+    GRAPH
+  </div>
+);
+
+const List = () => (
+  <div style={{ ...listLayoutStyle, backgroundColor: 'white', height: 600 }}>
+    LIST
+  </div>
+);
 
 export default () => (
   <div style={style}>
-    BODY
+    <Graph />
+    <Graph />
+    <Graph />
+    <List />
   </div>
 );
