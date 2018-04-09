fbbb3efcba92c041e31693b25b1f2a7cd8e8f86f
Evan Dean <evan.dean@intersection.com>
2018-04-08 21:13:51 -0400

    Flesh out List component a bit

--- src/List.jsx
+++ src/List.jsx
@@ -1,12 +1,57 @@
 import React from 'react';
 
-const defaultStyle = {
+const listItemsStyle = {
   backgroundColor: 'white',
-  height: 600,
 };
 
+const searchBarStyle = {
+  height: 80,
+};
+
+const SearchBar = () => (
+  <div style={searchBarStyle}>SEARCH BAR</div>
+);
+
+const listHeaderStyle = {
+  height: 60,
+  backgroundColor: '#e2e4e8',
+};
+
+const ListHeader = () => (
+  <div style={listHeaderStyle}>LIST HEADER</div>
+);
+
+const listFooterStyle = {
+  height: 80,
+};
+
+const ListFooter = () => (
+  <div style={listFooterStyle}>LIST FOOTER</div>
+);
+
+const listItemStyle = {
+  height: 80,
+};
+
+const ListItem = ({ index }) => (
+  <div style={{ ...listItemStyle, backgroundColor: index % 2 === 0 ? '#ffffff' : '#f7f8f9'}}>LIST ITEM</div>
+);
+
+const ListItems = () => (
+  <div style={listItemsStyle}>
+    <SearchBar />
+    <ListHeader />
+    {Array(10).fill().map((item, index) => (
+      <ListItem key={index} index={index} />
+    ))}
+    <ListFooter />
+  </div>
+);
+
 export default ({ style }) => (
-  <div style={{ ...defaultStyle, ...style }}>
-    LIST
+  <div style={{ ...style }}>
+    Active Content: 110 Items
+    <button>ADD NEW</button>
+    <ListItems />
   </div>
 );
